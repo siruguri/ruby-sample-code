c28c0ca4cecc99375707fb1694ca8afb159911cb
Sameer Siruguri <siruguri@gmail.com>
2013-10-09 22:40:49 -0700

    played ard with bindings

--- try_binding.rb
+++ try_binding.rb
@@ -1,10 +1,34 @@
-b=binding
+begin
+  puts not_defined
+rescue Exception => e
+  puts "Can variables be known before they are defined?"
+  puts "No, they can't."
+end
+puts "But they are listed in local_variables: #{local_variables}"
 
-a=1
-
-c= b.eval('local_variables')
-c.each do |var|
-  puts "Local variable: #{var}"
+not_defined=1
+begin
+  say_hello(not_defined)
+rescue Exception => e
+  puts "Can functions be known before they are defined?"
+  puts "No, they can't."
 end
 
+def say_hello(val)
+  puts val.to_s
+end
 
+class Sclass
+  def yxme
+    "y" + xme
+  end
+
+end
+def xme
+  "x"
+end
+#puts (Sclass.private_methods - Object.instance_methods).sort
+#puts Object.private_methods.sort
+s=Sclass.new
+#puts Sclass.private_methods.sort
+puts s.yxme
---
+++ try_module.rb
@@ -0,0 +1,6 @@
+module Namespace
+  a=1
+  puts self.name
+end
+
+puts self.inspect
