c6f6714c373ad0ba42a8b4611f506deb11ac79f7
Sameer Siruguri <siruguri@gmail.com>
2019-02-14 00:27:05 -0800

    Making week 2 email

--- projects/surveymonkey.rb
+++ projects/surveymonkey.rb
@@ -1,22 +1,39 @@
-require 'surveymonkey'
+require 'pry'
+require_relative 'surveymonkey/lib/surveymonkey'
 
-ENV['SURVEY_MONKEY_TOKEN'] = 'hBD-ASxyjkbtY-o5lr6R6olwGwOw.1vJ8pmrSh0OaEE95gpQf15lwXQxuH735DCCDZ8JGudsu5BzcVVaeohD2nYmMx7.8gst5zXzu33kXfEgdrm2NWQnIpIqyzMEcyCc'
-survey_id = 'Z2r2MNicKDG5rqqZsowNdFdcnVJu4gtzZPc9PzIYTKa600kj7_2BQxO0wqYaLr0oSz'
 client = SurveyMonkeyApi::Client.new
 
-surveys = client.surveys
-survey_id = surveys['data'][0]['id']
+# ELL API
+client.token = 'UNEvOFpSMgTjIsyO8bPLEiuujmRF-I5W6KNU2dwhLokmEapxiemkFLdagdDVOKJt50UwD.JiTm.Yw7k2XLCXe5DDplqLicPso3jwTjJjUESmxhBPvKXdyI5NJFvl.mo2'
 
-survey = client.survey_with_details(survey_id)
-collectors = client.collectors survey_id
-collector_id = collectors['data'].find { |i| i['name'] =~ /Shared link/ }['id']
-puts client.collector(collector_id)['url']
+# My API
+#client.token = 'hBD-ASxyjkbtY-o5lr6R6olwGwOw.1vJ8pmrSh0OaEE95gpQf15lwXQxuH735DCCDZ8JGudsu5BzcVVaeohD2nYmMx7.8gst5zXzu33kXfEgdrm2NWQnIpIqyzMEcyCc'
+
+surveys = client.surveys
+survey_id = surveys['data'][1]['id']
+puts surveys['data'][1]['title']
+puts client.pages(survey_id)
+puts client.questions(survey_id).map { |q| q.to_json }.compact
 exit
 
-responses = client.responses(survey_id)
+survey_pages = client.pages survey_id
+puts client.page_ids survey_id
+
+survey_pages.each do |page|
+  page.questions.each do |question|
+    puts question.details
+  end
+end
+exit
+responses = client.responses_with_details(survey_id)
+puts responses
+
 responses['data'].each do |response|
   response_id = response['id']
   response = client.response_with_details survey_id, response_id
   puts response['pages']
 end
 
+survey = client.survey_with_details(survey_id)
+collectors = client.collectors survey_id
+collector_id = collectors['data'].find { |i| i['name'] =~ /Shared link/ }['id']
