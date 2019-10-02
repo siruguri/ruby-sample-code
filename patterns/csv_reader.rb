6411ad1c52ff88209fd05bbef6ce944c5893917a
Sameer Siruguri <siruguri@gmail.com>
2019-10-02 11:44:07 -0700

    Update github client

--- patterns/csv_reader.rb
+++ patterns/csv_reader.rb
@@ -1,11 +1,29 @@
-require 'chronic'
 require 'csv'
+require 'pry'
 
-f = File.open ARGV[0]
-c = CSV.new f
-c.each() do |row|
-  if row.size > 1 and row[0]=~/20\d\d/
-    date = Chronic.parse(row[0])
-    puts date
-  end
+hash = {}
+index = 0
+
+def hist_key_list(keys)
+  # vendid,item,invoice,expped,entityid,ref,acctnum,itemamt,checkdt,checkno,checkpd,ckcashglref,addldesc
+  # vendid,item,invoice,expped
+  "#{keys[0]}+#{keys[1]}+#{keys[2]}+#{keys[3]}"
 end
+
+def ghis_key_list(keys)
+  # item,acctnum,period,ref,entityid,source,amt,pdentry,descrpn,addldesc,basis
+  # item,period,ref
+  "#{keys[0]}+#{keys[2]}+#{keys[3]}"
+end
+
+CSV.foreach(ARGV[0]) do |row|
+  puts index if index %100 == 0
+  index += 1
+  key_string = ARGV[1] ? hist_key_list(row) : ghis_key_list(row)
+  next if key_string.gsub('+', '').size == 0
+  if hash[key_string]
+    binding.pry
+  end
+  hash[key_string] = 1
+end
+
--- projects/github.rb
+++ projects/github.rb
@@ -1,42 +1,74 @@
 require 'pry'
 require 'github_api'
-token = ARGV[0]
 
-client = Github::Client::Issues.new oauth_token: token
+class Tracker
+  attr_reader :open_details, :today, :client, :token, :closed_details
 
-page_num = 1
-page_available = true
-today = DateTime.now
-
-def open?(i)
-  i.closed_at.nil?
-end
-
-def has_label?(i, label)
-  i.labels.select { |l| l.name.to_sym == label }.size > 0
-end
-
-while(page_available)
-  issues = client.list filter: 'all', repo: 'canopy', user: 'sunnyrjuneja', state: 'all',
-                       page: page_num
-  m = /page=(\d+)...rel..next/.match(issues.headers['Link'])
-  if m.nil? || m[1].nil?
-    page_available = false
-  else
-    page_num = m[1].to_i
+  def initialize
+    @token = ARGV[0]
+    @open_details = []
+    @closed_details = []
+    @client = Github::Client::Issues.new oauth_token: token
+    @today = DateTime.now
   end
-  issues.each do |i|
-    created_at = DateTime.strptime(i.created_at, '%Y-%m-%dT%H:%M:%SZ')
-    if has_label?(i, :bug)
-      if open?(i)
-        gap = (today - created_at).to_i
-        puts "#{i.number}: #{i.title}, created #{gap.to_f} days ago"
+
+  def open?(i)
+    i.closed_at.nil?
+  end
+
+  def has_label?(i, label)
+    i.labels.select { |l| l.name.to_sym == label }.size > 0
+  end
+
+  def run
+    page_available = true
+    page_num = 1
+
+    while(page_available)
+      issues = client.list filter: 'all', repo: 'canopy', user: 'sunnyrjuneja', state: 'all',
+                           page: page_num
+      m = /page=(\d+)...rel..next/.match(issues.headers['Link'])
+      if m.nil? || m[1].nil?
+        page_available = false
       else
-        closed_at = DateTime.strptime(i.closed_at, '%Y-%m-%dT%H:%M:%SZ')
-        gap = (closed_at - created_at).to_i
-        puts "#{i.number}: #{i.title}, closed in #{gap.to_f} days"
+        page_num = m[1].to_i
+      end
+      issues.each do |i|
+        created_at = DateTime.strptime(i.created_at, '%Y-%m-%dT%H:%M:%SZ')
+        if has_label?(i, :bug)
+          if open?(i)
+            gap = (today - created_at).to_i
+            open_details << {number: i.number, title: i.title, created_at: gap.to_f}
+          else
+            closed_at = DateTime.strptime(i.closed_at, '%Y-%m-%dT%H:%M:%SZ')
+            gap = (closed_at - created_at).to_i
+            closed_details << {number: i.number, title: i.title, closed_in: gap.to_f}
+          end
+        end
       end
     end
+
+    self
+  end
+
+  def print_details(params)
+    params.each do |param|
+      puts "## #{param.upcase}"
+      next if !self.respond_to?(param)
+      self.send(param).each do |detail|
+        print "\t"
+        print "##{detail[:number]} (#{detail[:title]}) - "
+        if detail[:closed_in]
+          print "closed in #{detail[:closed_in]}"
+        elsif detail[:created_at]
+          print "opened on #{detail[:created_at]}"
+        end
+        puts
+      end
+    end
+
+    self
   end
 end
 
+Tracker.new.run.print_details [:open_details]
